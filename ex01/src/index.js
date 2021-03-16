bbe9b629d041c835d1d50501f411d1ee959dd615
SplaY <mirza_messic@hotmail.com>
2021-03-16 17:32:35 +0100

    1st Boolean

---
+++ ex01/src/index.js
@@ -0,0 +1,9 @@
+// Only change code below this line
+function myTrueBoolean(){
+    return true;
+    }
+
+myTrueBoolean();
+// Only change code above this line
+console.log(myTrueBoolean());
+module.exports = myTrueBoolean;
